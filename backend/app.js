1248c5be2b52c09304406a3db9dd1acc7908d790
Rohit Gaur <115492552+RohitGgaur@users.noreply.github.com>
2025-09-26 15:29:15 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -94,7 +94,7 @@ app.use('*', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
